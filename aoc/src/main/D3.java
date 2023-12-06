846f3addb4e210e40980b6371ce7b0e3031f658c
Francois Gingras <francois.gingras89@gmail.com>
2023-12-05 19:42:31 -0500

    D4 completed

--- aoc/src/main/D3.java
+++ aoc/src/main/D3.java
@@ -15,7 +15,7 @@ public class D3 {
 
         // Pre-read all line
         List<String> lines = new ArrayList<String>();
-        for (String line; (line = reader.readLine()) != null;) {
+        for (String line; (line = reader.readLine()) != null; ) {
             lines.add(line);
         }
 
@@ -23,14 +23,15 @@ public class D3 {
         // Number share reference for every position.
         EntryPointer[][] grid = new EntryPointer[lines.size()][lines.get(0).length()];
         int lineNumber = 0;
-        for (String line: lines) {
+        for (String line : lines) {
             // Extract all number on a given line
             var listOfNumber = Arrays.stream(line.split("[.*\\-$%+&/@#=]")).map((str) -> {
                 Optional<NumberPointer> extract = Optional.empty();
                 try {
                     var trim = str.replaceAll("[^0-9]", "");
                     extract = Optional.of(new NumberPointer(Integer.valueOf(trim)));
-                } catch (Exception e) { }
+                } catch (Exception e) {
+                }
                 return extract;
             }).flatMap(Optional::stream).collect(Collectors.toList());
 
@@ -43,12 +44,12 @@ public class D3 {
                 }
             }
             var tempLine = line;
-            for (NumberPointer num: listOfNumber) {
+            for (NumberPointer num : listOfNumber) {
                 var numStr = num.number.toString();
                 var numSize = numStr.length();
                 var start = tempLine.indexOf(numStr);
                 for (int k = 0; k < numSize; k++) {
-                    gridLine[start+k] = num;
+                    gridLine[start + k] = num;
                 }
                 tempLine = tempLine.replaceFirst(numStr, new String(new char[numSize]).replace('\0', '.'));
             }
@@ -62,35 +63,35 @@ public class D3 {
         Long ratioSum = 0l;
         for (int i = 0; i < grid.length; i++) { // line
             for (int j = 0; j < grid[i].length; j++) { // column
-                if ((grid[i][j] instanceof SymbolPointer) && (((SymbolPointer)grid[i][j]).symbol() == '*')) {
+                if ((grid[i][j] instanceof SymbolPointer) && (((SymbolPointer) grid[i][j]).symbol() == '*')) {
                     // Lookup for number around
                     // a b c (i - 1)
                     // d x e
                     // f g h (i + 1)
                     Set<NumberPointer> adjacentPart = new HashSet<>();
 
-                   var a = safeTryGetNumber(grid, i-1, j-1);
-                   if (a != null) adjacentPart.add(a);
+                    var a = safeTryGetNumber(grid, i - 1, j - 1);
+                    if (a != null) adjacentPart.add(a);
 
-                    var b = safeTryGetNumber(grid, i-1, j);
+                    var b = safeTryGetNumber(grid, i - 1, j);
                     if (b != null) adjacentPart.add(b);
 
-                    var c = safeTryGetNumber(grid, i-1, j+1);
+                    var c = safeTryGetNumber(grid, i - 1, j + 1);
                     if (c != null) adjacentPart.add(c);
 
-                    var d = safeTryGetNumber(grid, i, j-1);
+                    var d = safeTryGetNumber(grid, i, j - 1);
                     if (d != null) adjacentPart.add(d);
 
-                    var e = safeTryGetNumber(grid, i, j+1);
+                    var e = safeTryGetNumber(grid, i, j + 1);
                     if (e != null) adjacentPart.add(e);
 
-                    var f = safeTryGetNumber(grid, i+1, j-1);
+                    var f = safeTryGetNumber(grid, i + 1, j - 1);
                     if (f != null) adjacentPart.add(f);
 
-                    var g = safeTryGetNumber(grid, i+1, j);
+                    var g = safeTryGetNumber(grid, i + 1, j);
                     if (g != null) adjacentPart.add(g);
 
-                    var h = safeTryGetNumber(grid, i+1, j+1);
+                    var h = safeTryGetNumber(grid, i + 1, j + 1);
                     if (h != null) adjacentPart.add(h);
 
                     if (adjacentPart.size() == 2) {
@@ -109,17 +110,23 @@ public class D3 {
     public static NumberPointer safeTryGetNumber(EntryPointer[][] grid, int di, int dj) {
         NumberPointer returnValue = null;
         try {
-            if (grid[di][dj] instanceof NumberPointer) returnValue = (NumberPointer)grid[di][dj];
-        } catch (Exception ex) { }
+            if (grid[di][dj] instanceof NumberPointer) returnValue = (NumberPointer) grid[di][dj];
+        } catch (Exception ex) {
+        }
         return returnValue;
     }
 }
 
-interface EntryPointer { }
-record SymbolPointer(Character symbol) implements EntryPointer { }
+interface EntryPointer {
+}
+
+record SymbolPointer(Character symbol) implements EntryPointer {
+}
+
 class NumberPointer implements EntryPointer {
     Integer number;
     Boolean isPartNumber;
+
     public NumberPointer(Integer iNumber) {
         number = iNumber;
         isPartNumber = false;
